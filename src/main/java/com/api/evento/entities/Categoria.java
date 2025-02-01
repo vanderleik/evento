c9ea75decbff3b4b5a5273dbc887e7836413e003
vanderleik <vanderleik@yahoo.com.br>
2025-02-01 10:42:26 -0300

    feat: criando a entidade Categoria

---
+++ src/main/java/com/api/evento/entities/Categoria.java
@@ -0,0 +1,54 @@
+package com.api.evento.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Table(name = "tb_categoria")
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
